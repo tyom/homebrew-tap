2c3ba524ea2b63ebdcc22a78f5d09ff6d86750d5
Tyom Semonov <inspiro@gmail.com>
2026-08-20 21:34:58 +0100

    feat: add git-owns formula

---
+++ Formula/git-owns.rb
@@ -0,0 +1,24 @@
+class GitOwns < Formula
+  desc "Line ownership per directory, top contributors first"
+  homepage "https://github.com/tyom/git-owns"
+  url "https://github.com/tyom/git-owns/releases/download/v0.1.0/git-owns"
+  version "0.1.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "MIT"
+
+  def install
+    bin.install "git-owns"
+  end
+
+  test do
+    assert_match "Usage: git owns", shell_output("#{bin}/git-owns -h")
+
+    # A tree to blame, since without a commit there is nothing to own
+    system "git", "-C", testpath, "init", "-q"
+    (testpath/"a.txt").write("one\ntwo\n")
+    system "git", "-C", testpath, "add", "a.txt"
+    system "git", "-C", testpath, "-c", "user.name=Ada Lovelace",
+           "-c", "user.email=ada@example.com", "commit", "-qm", "a"
+    assert_match "Ada L.", shell_output("cd #{testpath} && #{bin}/git-owns")
+  end
+end
